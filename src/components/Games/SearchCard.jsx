2692d01d63a501c568a96861984c6d499fa0d943
mohamadsaad1 <mohamad_utd@outlook.com>
2022-01-18 20:30:06 -0600

    add hover state

--- src/components/Games/SearchCard.jsx
+++ src/components/Games/SearchCard.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useState } from "react";
+
 
 const SearchCard=(props) =>{
+  const [hover, setHover] = useState(false)
   return(
-<div>
+<div 
+    className='gameCard'
+    onMouseEnter={()=>setHover(true)}
+    onMouseLeave={()=>setHover(false)}
+    >
   <div className="wrapper">
   <img className="game-image" src={props.game.background_image} alt='game' />
   </div>
