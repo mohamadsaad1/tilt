da6b0fc6fd29f09137610c08b8d7b40882d12044
mohamadsaad1 <mohamad_utd@outlook.com>
2022-01-26 20:04:38 -0600

    fix login css

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import * as authService from '../../services/authService'
+import "./LoginForm.module.css"
 
 const LoginForm = props => {
   const [formData, setFormData] = useState({
